fb4df03d0a921cf06d8462e42f7efe897a0d564b
nupurchauhan6 <nupur.chauhan6@gmail.com>
2022-07-03 00:49:52 -0400

    added support for upload file

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,11 @@ import path from "path";
 const app = express();
 const port = 3001;
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'))
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
-app.use('/api', routes);
+app.use("/api", routes);
 
 app.listen(port, () => {
   console.log(`Server started at http://localhost:${port} ....`);
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import images from "./api/image";
+import files from "./api/file";
 
 const routes = express.Router();
 
 routes.use('/images', images);
+routes.use('/files', files);
 
 export default routes;
